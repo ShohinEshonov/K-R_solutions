055b6995632c5ea787d8c9f1a61c52450a8cd1be
Shohin Eshonov <sohinesonov1@gmail.com>
2026-07-22 14:48:09 +0500

    Added solution for exercise-5.11 (but it dont handle negative number).

---
+++ chapter_5/exercise-5.10/stack.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+#define MAXVAL 2
+
+
+int sp = 0;
+double val[MAXVAL];
+
+void push(double f) 
+{
+	if(sp < MAXVAL)	
+		val[sp++] = f;
+	else
+		printf("error: stack full, cant push %g\n", f);
+}
+
+double pop(void)
+{
+	if(sp > 0)
+		return val[--sp];
+	else {
+		printf("error: stack empty\n");
+		return 0;
+	}
+
+}
+
+
+double top(void)
+{	
+	if(sp > 0)
+		return val[sp-1];
+	else{
+		printf("error: stack empty\n");
+		return 0.0;
+	}
+}
